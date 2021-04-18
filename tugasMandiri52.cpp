78b0a2759e3a6152da7cce4bd204e8deac12eb82
Dida Rizaldy <63806329+didarizaldy@users.noreply.github.com>
2021-04-18 11:17:02 +0700

    Praktek Dasar Komputert 2

--- tugasMandiri52.cpp
+++ tugasMandiri52.cpp
@@ -19,15 +19,18 @@ main()
 
     switch (jenis)
     {
-    case 'A':
+    case 'A': case 'a':
+        //diskon 10% untuk A
         diskon=10;
         Hdiskon=harga-(0.10*harga);
         break;
-    case 'B':
+    case 'B': case 'b':
+        //diskon 15% untuk B
         diskon=15;
         Hdiskon=harga-(0.15*harga);
         break;
-    case 'C':
+    case 'C': case 'c':
+        //diskon 20% untuk C
         diskon=20;
         Hdiskon=harga-(0.20*harga);
         break;
